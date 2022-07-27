601b420eff9a779c2fca9aa72c98ed1b5364ea4f
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2022-07-27 22:29:33 +0300

    slider_error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import Managed from './Components/Managed/CardCreate'
+import Slider from './Components/Slider'
+
+const db = [
+  { images: 'https://bipbap.ru/wp-content/uploads/2017/04/0_e7ae3_d81b7d27_orig.jpg' },
+  { images: 'https://klike.net/uploads/posts/2019-06/1559887616_1.jpg' },
+  {images:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhTRmNp8syGbJzT_SlLT2VjKGIiazK2mZRRcMFDssxNAT2K-bp5TkY7FBXvh0iwXmnbwk&usqp=CAU'}
+]
 
 export default function App() {
   return (
-    <Managed/>
+    <Slider images={db} />
   )
 }
-
---
+++ src/Components/Slider/index.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import Controller from './Controller';
+import styles from './Slider.style/Style.module.scss'
+
+
+export default function Slider(props) {
+    const [currentImgId, setCurrentImgId] = useState(0);
+    const {images} = props;
+    const setPrevSlide = () => {
+        currentImgId == 0 ? 
+        setCurrentImgId(images.length - 1) : 
+        setCurrentImgId(currentImgId - 1);
+    }
+    const setNextSlide = () => {
+        currentImgId == images.length-1 ? 
+        setCurrentImgId(0) : 
+        setCurrentImgId(currentImgId + 1);
+    }
+    return(
+        <div className={styles.container}>
+            <Slider images={images} currentImgId={currentImgId}/>
+            <Controller setPrevSlide={setPrevSlide} setNextSlide={setNextSlide} />
+        </div>
+    )
+}
+
+Slider.propTypes = {
+    images: PropTypes.array.isRequired,
+};
